41755c7cf2d8cca9df50efc5f290b63761c05168
zll600 <3400692417@qq.com>
2023-06-21 23:27:18 +0800

    feat(server): set schema in server

---
+++ server/src/schema.js
@@ -0,0 +1,29 @@
+const gql = require("graphql-tag");
+
+const typeDefs = gql`
+  # Schema definitions go here
+
+  "A track is a group of Modules that teaches about a specific topic"
+  type Track {
+    # Fields go here
+    id: ID!
+    title: String!
+    author: Author!
+    thumbnail: String
+    length: Int
+    modulesCount: Int
+  }
+
+  "Author of a complete Track or a Module"
+  type Author {
+    # Fields go here
+    id: ID!
+    name: String!
+    photo: String
+  }
+
+  type Query {
+    "Get tracks array for homepage grid"
+    tracksForHome: [Track!]!
+  }
+`;
